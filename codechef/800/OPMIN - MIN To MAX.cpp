5062bf4de16bff8f00bd44035b9cbb05ee4ea946
mallappwaghamore-png <mallappwaghamore@gmail.com>
2026-09-14 23:43:32 +0530

    Add MIN To MAX (800)

---
+++ codechef/800/OPMIN - MIN To MAX.cpp
@@ -0,0 +1,11 @@
+class Solution:
+    def count_non_minimum(self, nums):
+        minimum=min(nums)
+        count=0
+        for x in nums:
+            if x!=minimum:
+                count+=1
+        return count
+        # write your code here
+        
+
